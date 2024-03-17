c7194a5c659c959f1a584c7eb02aa559730cfc03
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-03-17 16:45:33 +0000

    Section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
@@ -14,11 +16,16 @@ const Header = () => {
     if (openNavigation) {
       setOpenNavigation(false);
     } else {
+      enablePageScroll();
       setOpenNavigation(true);
+      disablePageScroll();
     }
   };
 
   const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
     setOpenNavigation(false);
   };
 
